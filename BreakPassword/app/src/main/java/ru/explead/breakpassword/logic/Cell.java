78726d55bfedee176f5e6305c266be2424633c56
Alexander Efimov <teeeses@mail.ru>
2017-01-13 16:57:55 +0300

    create generator password

---
+++ BreakPassword/app/src/main/java/ru/explead/breakpassword/logic/Cell.java
@@ -0,0 +1,20 @@
+package ru.explead.breakpassword.logic;
+
+/**
+ * Created by develop on 13.01.2017.
+ */
+
+public class Cell {
+
+    private int value;
+
+    private Cell() {}
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
--- BreakPassword/app/src/main/java/ru/explead/breakpassword/logic/Controller.java
+++ BreakPassword/app/src/main/java/ru/explead/breakpassword/logic/Controller.java
@@ -1,12 +1,49 @@
 package ru.explead.breakpassword.logic;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by develop on 13.01.2017.
  */
 
 public class Controller {
 
-    public Controller() {
 
+    private int numberCells;
+    private ArrayList<Integer> password = new ArrayList<>();
+    private Random random = new Random();
+
+    public Controller() {
+        numberCells = 100;
+        generatePassword(numberCells);
+        for(int i = 0; i < password.size(); i++) {
+            Log.d("TAG", Integer.toString(password.get(i)));
+        }
+    }
+
+    public void generatePassword(int numberCells) {
+        this.numberCells = numberCells;
+        for(int i = 0; i < numberCells; i++) {
+            int number;
+            do {
+                number = random.nextInt(9);
+            } while (password.contains(number));
+            password.add(number);
+        }
+    }
+
+    public ArrayList<Integer> getPassword() {
+        return password;
+    }
+
+    public int getNumberCells() {
+        return numberCells;
+    }
+
+    public void setNumberCells(int numberCells) {
+        this.numberCells = numberCells;
     }
 }
