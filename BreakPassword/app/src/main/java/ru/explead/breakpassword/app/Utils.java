24093d4c21c00b5b5c1174766f28451d6272d1a8
Tukzar333 <everlast.isi@gmail.com>
2019-07-21 00:15:58 +0300

    рефакторинг

--- BreakPassword/app/src/main/java/ru/explead/breakpassword/app/Utils.java
+++ BreakPassword/app/src/main/java/ru/explead/breakpassword/app/Utils.java
@@ -17,7 +17,7 @@ public class Utils {
     public final static String BEST_HARD ="best_hard";
     public final static String BEST_VERY_HARD = "best_very_hard";
 
-    public static final String APP_PREFERENCES = "mysettings";
+    public static final String APP_PREFERENCES = "settings";
 
     public static Typeface getTypeFaceLevel(Context context) {
         return Typeface.createFromAsset(context.getAssets(),"fonts/level_personal.ttf");
